d636ac911a1b93b066d7aefbce4649db566b91a4
Naty S <noreply@eclecticpassions.net>
2026-01-15 14:57:37 +0800

    Feat: Add /ping path to return 200 OK for cron-job.org
    - To stop Render from sleeping server after 15 min of inactivity

--- api/index.js
+++ api/index.js
@@ -4,6 +4,13 @@ import config from "../indiekitrc";
 const app = indiekit(config);
 
 export default async function (req, res) {
+  // simple health check that bypasses indiekit and returns 200 OK
+  if (req.url === "/ping" || req.url === "/health") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.end("OK");
+    return;
+  }
   return new Promise((resolve) => {
     app(req, res, () => resolve());
   });
